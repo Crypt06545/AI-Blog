f20e7c69a8bccae38eb72b0ae7661be0ff8878e8
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-06-06 16:23:43 +0600

    add animation on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,30 +4,51 @@ import React from "react";
 import animationData from "@/app/assets/ai-animation.json";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
+  const tl = gsap.timeline();
+  useGSAP(() => {
+    tl.from(".info-sec", {
+      y: -30,
+      opacity: 0,
+      duration: 0.86,
+      delay: 0.25,
+    });
+    tl.from(".info-sec-button", {
+      opacity: 0,
+      duration: 0.88,
+    });
+    tl.from('.animate-svg',{
+      scale:0,
+      opacity:0
+    },'-=1')
+  }, []);
   return (
     <section className="w-full px-6 md:px-16 lg:px-24 xl:px-32 min-h-screen text-white flex flex-col-reverse lg:flex-row items-center justify-between gap-10 py-20">
       {/* Left content */}
-      <div className="max-w-xl text-center lg:text-left space-y-6">
-        <h1 className="text-4xl md:text-5xl font-extrabold leading-tight text-balance">
-          Unleashing the Power of <br />
-          <span className="bg-gradient-to-r from-blue-400 to-cyan-400 bg-clip-text text-transparent">
-            AI
-          </span>{" "}
-          Through Words
-        </h1>
-        <p className="text-gray-400 text-base leading-relaxed tracking-wide">
-          Dive into a world of intelligence, ethics, and innovation — explained
-          through engaging blogs and real-world insights.
-        </p>
-        <div className="flex flex-col sm:flex-row items-center gap-4 justify-center lg:justify-start">
-          <Link href={'/blog'}>
-            <Button className="bg-blue-400 hover:bg-blue-500 text-white w-40 h-11 rounded-full text-sm transition-all duration-200">
+      <div className=" max-w-xl text-center lg:text-left space-y-6">
+        <div className="info-sec">
+          <h1 className="text-4xl md:text-5xl font-extrabold leading-tight text-balance">
+            Unleashing the Power of <br />
+            <span className="bg-gradient-to-r from-blue-400 to-cyan-400 bg-clip-text text-transparent">
+              AI
+            </span>{" "}
+            Through Words
+          </h1>
+          <p className="text-gray-400 text-base leading-relaxed tracking-wide">
+            Dive into a world of intelligence, ethics, and innovation —
+            explained through engaging blogs and real-world insights.
+          </p>
+        </div>
+        <div className="info-sec-button flex flex-col sm:flex-row items-center gap-4 justify-center lg:justify-start">
+          <Link href={"/blog"}>
+            <Button className="cursor-pointer bg-blue-400 hover:bg-blue-500 text-white w-40 h-11 rounded-full text-sm transition-all duration-200">
               Explore Blog
             </Button>
           </Link>
-          <Button className="border border-blue-300 text-blue-300 hover:bg-blue-500 hover:text-white w-40 h-11 rounded-full text-sm transition-all duration-200">
+          <Button className="border cursor-pointer border-blue-300 text-blue-300 hover:bg-blue-500 hover:text-white w-40 h-11 rounded-full text-sm transition-all duration-200">
             Subscribe
           </Button>
         </div>
@@ -36,7 +57,7 @@ const Hero = () => {
       {/* Animation */}
       <div className="max-w-md w-full">
         <Lottie
-          className="w-full"
+          className="animate-svg w-full"
           animationData={animationData}
           loop
           autoPlay
