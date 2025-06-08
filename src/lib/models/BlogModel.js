f1e75c13efec5463bab0d97ffa27d95775ea9c93
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-06-08 19:14:48 +0600

    create schema and db connection

---
+++ src/lib/models/BlogModel.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const BlogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  authorImg: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Blog = mongoose.model("Blog", BlogSchema);
+export default Blog;
